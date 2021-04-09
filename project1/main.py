61708b7126b510d95f50922e3fc6f2a2d891b7c8
ChrisNguyen99 <32182141+ChrisNguyen99@users.noreply.github.com>
2021-04-09 09:17:27 -0400

    Update main.py

--- project1/main.py
+++ project1/main.py
@@ -6,28 +6,28 @@ import glob
 import numpy
 import spacy
 
-def main(input, concept, output, stats, names, genders, dates, phones):
+#def main(input, concept, output, stats, names, genders, dates, phones):
     
-    nlp = spacy.load('en')
+    #nlp = spacy.load('en')
     
-    files = glob.glob(input)
+    #files = glob.glob(input)
     
-    for file in files:
-        fi = open(file)
-        text = fi.read()
-        doc = nlp(text)
-        doc
+    #for file in files:
+        #fi = open(file)
+        #text = fi.read()
+        #doc = nlp(text)
+        #doc
         
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--input", type=str, required=True, help="input txt files")
-    parser.add_argument("--names", type=bool, required=True, help="names flag")
-    parser.add_argument("--genders", type=bool, required=True, help="genders flag")
-    parser.add_argument("--dates", type=bool, required=True, help="dates flag")
-    parser.add_argument("--phones", type=bool, required=True, help="phone numbers flag")
-    parser.add_argument("--concept", type=str, required=True, help="concept specified")
-    parser.add_argument("--output", type=str, required=True, help="output file")
-    parser.add_argument("--stats", type=str, required=True, help="stats file")
+#if __name__ == '__main__':
+    #parser = argparse.ArgumentParser()
+    #parser.add_argument("--input", type=str, required=True, help="input txt files")
+    #parser.add_argument("--names", type=bool, required=True, help="names flag")
+    #parser.add_argument("--genders", type=bool, required=True, help="genders flag")
+    #parser.add_argument("--dates", type=bool, required=True, help="dates flag")
+    #parser.add_argument("--phones", type=bool, required=True, help="phone numbers flag")
+    #parser.add_argument("--concept", type=str, required=True, help="concept specified")
+    #parser.add_argument("--output", type=str, required=True, help="output file")
+    #parser.add_argument("--stats", type=str, required=True, help="stats file")
     
-    args = parser.parse_args()
-    main(args.input, args.concept, args.output, args.stats, args.names, args.genders, args.dates, args.phones)
+    #args = parser.parse_args()
+    #main(args.input, args.concept, args.output, args.stats, args.names, args.genders, args.dates, args.phones)
